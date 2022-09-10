c5a1096d0304ead0d4f8fe0aa89bbb8ea5a93e64
Aaliya313 <aaliyaladha@hotmail.co.uk>
2022-09-10 20:02:26 +0100

    Practicing how to use GitHub

---
+++ src/github2.java
@@ -0,0 +1,7 @@
+public class github2 {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("i am practicing how to use GitHub");
+    }
+    }
+}
